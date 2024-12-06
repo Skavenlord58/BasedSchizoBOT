e16f04280cf7329eb5ec83cf0cd29eb9a5ba144d
SkavenLord58 <adamkrasa@sd58.cz>
2024-12-06 01:27:48 +0100

    MOD: changes

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ async def on_message(m: Message):
                 else:
                     await m.reply(CESKA_LINUX_COPYPASTA)
         if "hilfe" in m.content.lower() or "pomoc" in m.content.lower() and "pomocí" not in m.content.lower():
-            await m.reply(f'''
+            if random.randint(0, 4) == 1:
+                await m.reply(f'''
             „{MOT_HLASKY[random.randint(0, len(MOT_HLASKY) - 1)]}“
                                                                                 - Mistr Oogway, {random.randint(470,480)} př. n. l.
             ''')
@@ -83,8 +84,8 @@ async def on_message(m: Message):
             await m.reply(
                 "Neklikejte na odkazy s názvem FREE PRIMOS. Obvykle toto bývá phishing scam. https://www.avast.com/cs-cz/c-phishing")
         if "jsem" in m.content.lower():
-            if random.randint(0, 32) == 4:
-                kdo = " ".join(m.content.split("jsem")[1].split(" ")[1:])
+            if random.randint(0, 36) == 4:
+                kdo = " ".join(m.content.split("jsem")[1].split(".")[0].split(",")[0].split(" ")[1:])
                 await m.reply(f'Ahoj, {kdo}. Já jsem táta.')
         if m.content.lower() == "kdo":
             await m.channel.send(f'kdo se ptal?')
@@ -108,10 +109,12 @@ async def on_message(m: Message):
             if random.randint(0, 16) == 4:
                 kdo = " ".join(m.content.split("jsi")[1].split(" ")[1:])
                 await m.reply(f'Tvoje máma je {kdo}.')
-        if random.randint(0, 169696969) == 1:
+        if random.randint(0, 69) == 1:
             await m.reply(f'mňau')
         if random.randint(0, 500000) == 1:
             await m.reply(f'pipík')
+        if random.randint(0, 200) == 1:
+            await m.reply(f'víš co? raději drž hubu, protože z tohohle jsem chytil rakovinu varlat')
         if "mama" in m.content.lower() or \
             "máma" in m.content.lower() or \
             "mami" in m.content.lower() or \
@@ -120,7 +123,7 @@ async def on_message(m: Message):
             "mamko" in m.content.lower():
             if random.randint(0, 64) == 1:
                 try:
-                    apiCall = requests.get("https://api.yomomma.info/")
+                    apiCall = requests.get("https://www.yomama-jokes.com/api/v1/jokes/random/")
                     if apiCall.status_code == 200:
                         await m.reply(f'{apiCall.json()["joke"]}')
                 except Exception as exc:
@@ -132,7 +135,30 @@ async def on_message(m: Message):
         if "crab rave" in m.content.lower():
             await m.reply(f"https://youtu.be/LDU_Txk06tM?t=75")
         if "já jo" in m.content.lower():
-            await m.reply(f"já ne")
+            if random.randint(0, 16) == 1:   
+                await m.reply(f"já ne")
         if "já ne" in m.content.lower():
-            await m.reply(f"já jo")
+            if random.randint(0, 16) == 1:   
+                await m.reply(f"já jo")
+        if "chci se zabít" in m.content.lower() or "suicidal" in m.content.lower():
+            await m.reply(f"omg don't kill yourself, ur too sexy, haha :catcry:")
+        if "v píči" in m.content.lower():
+            await m.reply(f"stejně tak moc v píči jako já včera večer v tvojí mámě loool :kekWR:")
+        if "buisness" in m.content.lower() \
+            or "bussines" in m.content.lower() \
+            or "bussiness" in m.content.lower() \
+            or "buissnes" in m.content.lower() \
+            or "buisnes" in m.content.lower():
+            await m.reply(f"KÁMO lmao ukažte si na toho blbečka, co neumí napsat 'business' XDDDD :index_pointing_at_the_viewer: příště raději napiš 'byznys' dík :)")
+        if "reminder" in m.content.lower():
+            if random.randint(0, 4) == 1:
+                await m.reply(f"kind reminder: ur a bitch :)")
+        if "youtu.be" in m.content.lower() or "youtube.com" in m.content.lower():
+            if random.randint(0, 5) == 1:
+                if bool(random.getrandbits(1)):
+                    await m.reply(f"recenze: strašnej banger")
+                else:
+                    await m.reply(f"recenze: cringe ass hovno")
+        if m.content.__len__() >= 600:
+            await m.reply(f"i ain't reading all of that. im happy for you tho, or sorry that happened. depends on you")
 client.run(TOKEN)
